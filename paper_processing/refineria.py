cf96d518911db6410623850edcb859b91e912e01
Eddie <efarfand@hotmail.com>
2025-09-15 19:44:40 -0600

    update for papers filters

---
+++ paper_processing/refineria.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import os
+# Set up the path and read the file into the script.
+path = r"D:\AAA\2. Estudios\Ingeniería en Biotecnología\Proyecto Terminal\Pipelines\data\papers\papers.jsonl"
+papers = pd.read_json(path, lines=True)
+
+#Creating safe copy
+work = papers.copy(deep=True)
+
+#let's find out if we have duplicated papers.
+# check for same PMID
+
+'''
+duplicated_pmid = work[work["PMID"].duplicated(keep=False)]
+print(duplicated_pmid.shape)
+print(duplicated_pmid.head())
+'''
+#remove all the duplicated ones, by default it keeps only the first instance.
+work = work.drop_duplicates(subset=["PMID"])
+print("Numer of duplicated PMIDs:")
+print(work["PMID"].duplicated().sum())
+#print(work.shape)
+#print(work.loc[0:5, "Title"])
+
+#first we will normalize all titles so it's easier to filter through them.
+work["Title"] = work["Title"].str.title()
+
+
+#Now lets remove the undesired paper.
+#We will do that by using a list of keywords that of no interest to us.
+
+unwanted_keywords = ['Dental',
+                    'Cancer',
+                    'Mouse',
+                    'Rat',
+                    'Mice',
+                    'Drugs',
+                    'Drug',
+                    'Assessment',
+                    'Ct',
+                    'Surgery',
+                    'Magnetic',
+                    'Imagining',
+                    'Mri',
+                    'Vitamin',
+                    'Vitamin',
+                    'Herbal',
+                    'Covid',
+                    'Nutritional',
+                    'Tumor',
+                    'Acupuncture',
+                    'Pharmacologic',
+                    'Pharmacology',
+                    'Postoperative',
+                    'Coffee',
+                    'Coping',
+                    'Management',
+                    'Diet',
+                    'Oral',
+                    'Heart',
+                    'Stroke',
+                    'Carcinogenesis',
+                    'Oil',
+                    'Psychological',
+                    'Safety',
+                    'Rhinitis',
+                    'Consumption',
+                    'Dosing',
+                    'Coronavirus',
+                    'Radiation',
+                    'Vaccination',
+                    'Violence',
+                    'Telemedicine',
+
+
+                    ]
+
+#now we detect which articles have the unwanted words. and will add a tag into another column to identify them.
+def contains_bad(title, keywords):
+    return any(keywords in title for keywords in keywords)
+
+work["unwanted"] = work["Title"].apply(lambda x: contains_bad(x, unwanted_keywords))
+
+#then we split it
+dumpster = work[work["unwanted"]].copy()
+work = work[~work["unwanted"]].copy()
+
+print("Kept:", len(work), " → Dumpster:", len(dumpster))
+
+#custumize the path
+dumpster_path = r"D:\AAA\2. Estudios\Ingeniería en Biotecnología\Proyecto Terminal\Pipelines\data\papers\dumpster\dumpster.csv"
+
+def save_to_dumpster(df, path):
+    if not os.path.exists(path):
+        # create new file with header if the file doesn't exists
+        df.to_csv(path, index=False, mode="w", header=True)
+        print(f"Created new dumpster file with {len(df)} rows → {path}")
+        return
+    else:
+        # append rows without writing header to an existing file THIS SHOULD BE APPEND BUT CHANGED CUZ IT WAS ACCUMULATING AND WAS TOO LAZY TO FIX IT
+        df.to_csv(path, index=False, mode="w", header=False)
+        print(f"Appended {len(df)} rows to existing dumpster → {path}")
+        return
+
+#The file with unwanted papers is created
+save_to_dumpster(dumpster, dumpster_path)
+
+
+
+print(f"This is the info for work: {work.shape}")
+print(f"This is the info for dumpster: {dumpster.shape}")
+#print(f"This is the info for dumpster: {dumpster.shape}")
+
+"""
+    At this point we already separated most of the unrelated papers, now we will continue to do the opposite
+what will do is to create a list of the most useful keywords and create a dataset with the most useful papers
+"""
+useful_keywords = [
+    'MiRNA',
+    'MicroARN',
+    'Prediction',
+    'IBD',
+    'Markers',
+    'Biomarkers',
+    'Artificial',
+    'Intelligence',
+    ]
+
+#now we detect which articles have the useful words. and will add a tag into another column to identify them.
+def contains_useful(title, keywords):
+    return any(keywords in title for keywords in keywords)
+
+work["wanted"] = work["Title", "Abstract"].apply(lambda x: contains_useful(x, useful_keywords))
+
+#then we split it
+wanted = work[work["wanted"]].copy()
+work = work[~work["wanted"]].copy()
+
+print("Kept:", len(work), " → Useful:", len(wanted))
+
+#Temporary creation for visualization purposes of work dataset
+save_to_dumpster(work, r"D:\AAA\2. Estudios\Ingeniería en Biotecnología\Proyecto Terminal\Pipelines\data\papers\dumpster\temporary.csv")
--- pipelines/papersrch/README.md
+++ pipelines/papersrch/README.md
@@ -1,10 +1,10 @@
 # Proyecto-Terminal
 
 ## Última búsqueda
-- Término: ("microRNA") AND ("celiac disease") AND ("inflammatory bowel disease") AND (Review[Publication Type] OR Systematic Review[Publication Type] OR Meta-Analysis[Publication Type])
-- Artículos guardados: 0
+- Término: ("Prediction") OR ("microARN") OR ("IBD") AND (Review[Publication Type] OR Systematic Review[Publication Type] OR Meta-Analysis[Publication Type])
+- Artículos guardados: 199
 
-Generado el 2025-09-11 10:15:27 UTC con versión 0.2.
+Generado el 2025-09-13 10:19:42 UTC con versión 0.2.
 
 ## Environment Variables
 
